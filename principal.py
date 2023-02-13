9a18650a3c7411b7b08f31d719e7d17ffadfe3a0
silva <silva.rodrigo31@outlook.com>
2023-02-12 22:06:50 -0300

    teste

--- principal.py
+++ principal.py
@@ -1,107 +1,26 @@
-from dash import dcc, html, Output, Input, State
-from paginas.pagina_apresentacao import pagina_apresentacao
-from paginas.lista_projetos import lista_projetos
-import dash
-import dash_bootstrap_components as dbc
+from dash import Dash, dcc, html, Input, Output
+import os
 
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.config['suppress_callback_exceptions'] = True
-app.scripts.config.serve_locally = True
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
 server = app.server
 
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
+])
 
-navbar_principal = dbc.Row(
-    [
-        dbc.Nav(
-            children=[
-                dbc.NavItem(
-                    dbc.NavLink(
-                        'Inicio',
-                        href='/paginas/pagina_apresentacao',
-                        id='id_pg_inicio',
-                        style={'color': '#FFFFFF'}
-                    )
-                ),
-                dbc.NavItem(
-                    dbc.NavLink(
-                        'Galeria de Projetos',
-                        href='/paginas/lista_projetos',
-                        id='id_pg_projetos',
-                        style={'color': '#FFFFFF'}
-                    )
-                )
-            ]
-        )
-    ],
-    className='g-0 ms-auto flex-nowrap mt-3 mt-md-0',
-    id='id_nav_main'
-)
-
-app.layout = html.Div(
-    [
-        dcc.Location(id='url'),
-        dbc.Navbar(
-            dbc.Container(
-                [
-                    html.A(
-                        dbc.Row(
-                            [
-                                dbc.Col(
-                                    dbc.NavbarBrand(
-                                        'Portifolio Rodrigo Silva Rocha',
-                                        className='ms-2',
-                                        style={'color': '#FFFFFF'}
-                                    )
-                                ),
-                            ],
-                            align='center',
-                            className='g-0',
-                        ),
-                    ),
-                    dbc.NavbarToggler(
-                        id='navbar-toggler',
-                        n_clicks=0
-                    ),
-                    dbc.Collapse(
-                        navbar_principal,
-                        id='navbar-collapse',
-                        is_open=False,
-                        navbar=True,
-                    ),
-                ]
-            ),
-            style={},
-            color='#141A32',
-            id='id_container'
-        ),
-        html.Div(id='page-content')
-    ]
-)
-
-
-@app.callback(
-    Output('navbar-collapse', 'is_open'),
-    [Input('navbar-toggler', 'n_clicks')],
-    [State('navbar-collapse', 'is_open')],
-)
-def toggle_navbar_collapse(n, is_open):
-    if n:
-        return not is_open
-    return is_open
-
-
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def render_page_content(pathname):
-    print('pathname', pathname)
-    if pathname == '/paginas/pagina_apresentacao' or pathname == '/':
-        return pagina_apresentacao
-    elif pathname == '/paginas/lista_projetos':
-        return lista_projetos
-    else:
-        return '404'
-
+@app.callback(Output('display-value', 'children'),
+                [Input('dropdown', 'value')])
+def display_value(value):
+    return f'You have selected {value}'
 
 if __name__ == '__main__':
-    app.run_server(port=8051, debug=True)
+    app.run_server(debug=True)
