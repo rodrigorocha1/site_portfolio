52d177a6c21251605a16ae290c0e04eb5e8e8f16
silva <silva.rodrigo31@outlook.com>
2023-02-16 22:05:09 -0300

    correção

--- pagina_principal.py
+++ pagina_principal.py
@@ -24,6 +24,7 @@ class PaginaPrincipal:
         self.app.layout = self.gerar_layout_principal()
         self._criar_calback_page()
 
+
     def gerar_layout_principal(self):
         return html.Div(
             [
@@ -129,6 +130,10 @@ class PaginaPrincipal:
         )(self._toggle_navbar_collapse)
 
 
-pp = PaginaPrincipal()
-pp.rodar_aplicacao()
-server = pp.server
+if __name__ == '__main__':
+    pp = PaginaPrincipal()
+    pp.rodar_aplicacao()
+    server = pp.server
+
+
+
