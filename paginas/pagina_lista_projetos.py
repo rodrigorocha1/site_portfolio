afbd87260532cc848311b215403d0309b4276236
silva <silva.rodrigo31@outlook.com>
2023-02-18 00:25:29 -0300

    Inversão da ordem das legendas de projetos

--- paginas/pagina_lista_projetos.py
+++ paginas/pagina_lista_projetos.py
@@ -9,16 +9,19 @@ class PaginaListaProjetos:
             Construtor com os parámetros
         """
         self._legendas_projetos = [
-            ('', 'id_img1', 'Pokedex Primeira Versão', 'id_pokedex_primeira_versao',
-             'Pokedex V1 feita, sem controle de repetição de código', 'id_desc_pokedex', 'link',
-             'id_link_pokedex'),
             ('', 'id_img2', 'Titulo', 'id_titulo2', 'desc', 'id_desc2', 'link', 'id_link_2'),
             ('', 'id_img3', 'Titulo', 'id_titulo3', 'desc', 'id_desc3', 'link', 'id_link_3'),
             ('', 'id_img4', 'Titulo', 'id_titulo4', 'desc', 'id_desc4', 'link', 'id_link_4'),
             ('', 'id_img5', 'Titulo', 'id_titulo5', 'desc', 'id_desc5', 'link', 'id_link_5'),
             ('', 'id_img6', 'Titulo', 'id_titulo6', 'desc', 'id_desc6', 'link', 'id_link_6'),
             ('', 'id_img7', 'Titulo', 'id_titulo7', 'desc', 'id_desc7', 'link', 'id_link_7'),
-            ('', 'id_img8', 'Titulo', 'id_titulo8', 'desc', 'id_desc8', 'link', 'id_link_8')
+            ('', 'id_img8', 'Titulo', 'id_titulo8', 'desc', 'id_desc8', 'link', 'id_link_8'),
+            ('', 'id_img1', 'Pokedex Primeira Versão',
+             'id_pokedex_primeira_versao',
+             'Pokedex V1 feita, sem controle de repetição de código, ou seja, o padrão de código aqui, viola os '
+             'principios SOLID',
+             'id_desc_pokedex', 'link',
+             'id_link_pokedex'),
         ]
         self.layout_lista_projetos = self._gerar_lista_projetos()
 
