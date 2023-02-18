161792814228c24d0e055f4ecbfbf40095d7fd7a
silva <silva.rodrigo31@outlook.com>
2023-02-18 11:12:52 -0300

    Passando para list comprehensions o método _gerar_lista_projetos

--- paginas/pagina_lista_projetos.py
+++ paginas/pagina_lista_projetos.py
@@ -16,12 +16,6 @@ class PaginaListaProjetos:
             ('', 'id_img6', 'Titulo', 'id_titulo6', 'desc', 'id_desc6', 'link', 'id_link_6'),
             ('', 'id_img7', 'Titulo', 'id_titulo7', 'desc', 'id_desc7', 'link', 'id_link_7'),
             ('', 'id_img8', 'Titulo', 'id_titulo8', 'desc', 'id_desc8', 'link', 'id_link_8'),
-            ('', 'id_img1', 'Pokedex Primeira Versão',
-             'id_pokedex_primeira_versao',
-             'Pokedex V1 feita, sem controle de repetição de código, ou seja, o padrão de código aqui, viola os '
-             'principios SOLID',
-             'id_desc_pokedex', 'link',
-             'id_link_pokedex'),
         ]
         self.layout_lista_projetos = self._gerar_lista_projetos()
 
@@ -30,38 +24,39 @@ class PaginaListaProjetos:
         Método para gerar a lista de cartões para a galeria
         @return: uma linha com cartões
         """
-        cartoes_projeto = []
-        for caminho_img, id_img, titulo_cartao, id_titulo, desc, id_desc, link, id_link in self._legendas_projetos:
-            card_lista_projeto = \
+
+        cartoes_projeto = [
+            dbc.Col(
                 dbc.Card(
-                    [
-                        dbc.CardImg(
-                            src=caminho_img,
-                            top=True,
-                        ),
-                        dbc.CardBody(
-                            [
-                                html.H4(
-                                    titulo_cartao,
-                                    id=id_titulo,
-                                    className='card-title'
-                                ),
-                                html.P(
-                                    desc,
-                                    id=id_desc,
-                                    className='card_text',
-                                )
-                            ]
-                        )
-                    ], className='class_coluna_info'
-                )
-            cartoes_projeto.append(
-                dbc.Col(
-                    card_lista_projeto,
-                    md=3,
-                    className='class_card_projeto'
+                    dbc.Card(
+                        [
+                            dbc.CardImg(
+                                src=caminho_img,
+                                top=True,
+                            ),
+                            dbc.CardBody(
+                                [
+                                    html.H4(
+                                        titulo_cartao,
+                                        id=id_titulo,
+                                        className='card-title'
+                                    ),
+                                    html.P(
+                                        desc,
+                                        id=id_desc,
+                                        className='card_text',
+                                    ),
+                                    dbc.CardLink("External link", href='../paginas/projetos/pokedex_primeira_versao'),
+                                ]
+                            )
+                        ], className='class_coluna_info'
+                    )
                 ),
-            )
+                md=3,
+                className='class_card_projeto'
+            ) for caminho_img, id_img, titulo_cartao, id_titulo, desc, id_desc, link, id_link in self._legendas_projetos
+        ]
+
         return html.Div(
             [
                 dbc.Row(
